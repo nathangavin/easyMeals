b236fb22deaab6e458c774794154de4b7465aa66
Nathan Gavin <nathangavin987@gmail.com>
2024-03-25 15:41:53 -0600

    updated Db dockerfile to auto generate tables on db startup using init.sql file

---
+++ db/init.sql
@@ -0,0 +1,102 @@
+USE easyMeals;
+
+CREATE TABLE IF NOT EXISTS Units (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Pantries (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS User (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(20) NOT NULL,
+    lastname VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    passwordHash VARCHAR(256) UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Recipes (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Ingredients (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    unitID INT NOT NULL,
+
+    CONSTRAINT i_unit
+    FOREIGN KEY(unitID) 
+    REFERENCES Units(ID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS IngredientQuantities (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ingredientID INT NOT NULL,
+    quantity INT NOT NULL,
+
+    CONSTRAINT q_ingredient
+    FOREIGN KEY(ingredientID)
+    REFERENCES Ingredients(ID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Instructions (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(256) NOT NULL,
+
+)
+
+CREATE TABLE IF NOT EXISTS InstructionIngredientQuantities (
+    quantityID INT NOT NULL,
+    instructionID INT NOT NULL,
+
+    CONSTRAINT i_quantity
+    FOREIGN KEY(quantityID) 
+    REFERENCES IngredientQuantities(ID)
+    ON DELETE CASCADE,
+
+    CONSTRAINT i_instruction
+    FOREIGN KEY(instructionID)
+    REFERENCES Instructions(ID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS PantryIngredientQuantities (
+    quantityID INT NOT NULL,
+    pantryID INT NOT NULL,
+
+    CONSTRAINT p_quantity
+    FOREIGN KEY(quantityID) 
+    REFERENCES IngredientQuantities(ID),
+
+    CONSTRAINT p_pantry
+    FOREIGN KEY(pantryID)
+    REFERENCES Pantries(ID)
+);
+
+CREATE TABLE IF NOT EXISTS UserPantries (
+    userID INT NOT NULL,
+    pantryID INT NOT NULL,
+
+    CONSTRAINT u_user
+    FOREIGN KEY(userID)
+    REFERENCES User(ID)
+    ON DELETE CASCADE,
+
+    CONSTRAINT u_pantry
+    FOREIGN KEY(pantryID)
+    REFERENCES Pantries(ID)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS UserRecipes (
+    ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userID INT NOT NULL,
+    recipeID INT NOT NULL,
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 10)
+);
