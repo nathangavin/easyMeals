34c3582b9906e1c53309dcf6772c97fc26139424
Nathan Gavin <nathangavin987@gmail.com>
2024-08-09 15:39:56 -0600

    refactored token system into seperate session related files, changed SQL accordingly

--- testing/index.js
+++ testing/index.js
@@ -9,6 +9,17 @@ import {
 main();
 
 async function main() {
+    /*
+    * =======================
+    * person creates new account
+    * User is created in DB
+    * person is forced to login
+    * login session token is created in DB and linked to User
+    * session token is returned and stored in cookie
+    * person logs out
+    * log out request is authenticated
+    * session token is deleted from DB
+    */
     console.log('Starting User Login Test');
     const res = await testCreateUser();
     handleTestCreateUser(res);
