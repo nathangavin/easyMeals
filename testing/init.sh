383266ba35651400d09e8d97c123f8ad24cb20ec
Nathan Gavin <nathangavin987@gmail.com>
2024-09-14 14:13:29 +1200

    working on setting up user for healthcheck

--- testing/init.sh
+++ testing/init.sh
@@ -1,3 +1,34 @@
 #!/usr/bin/env bash
 
+if ( ! pgrep -f docker > /dev/null ); then 
+    # docker is not running
+    echo "docker daemon not running. Start with 'dockerd'"
+    exit 1
+fi
 
+# move to testing dir if not there
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+
+# move to project root dir
+cd ..
+
+# stop compose network
+docker compose down
+
+# build new network
+docker compose build
+
+# start network in detached mode
+docker compose up -d
+
+echo "Waiting for services to be healthy..."
+docker compose exec -T db sh -c "until mysqladmin ping -h localhost --slient; do sleep 1; done"
+docker compose exec -T backend sh -c "until curl -f http://localhost:3000/health; do sleep 1; done"
+
+# run test suite
+cd ./testing 
+npm run start
+
+# shut down network
+docker compose down
